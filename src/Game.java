0a8849f872ec33f6d24ab25a0bc9f7391b581321
BIGOLDTURKEY <gubbermcbubbers@gmail.com>
2023-09-26 12:23:30 -0400

    If user enters number for letter it asks again

--- src/Game.java
+++ src/Game.java
@@ -26,33 +26,32 @@ public class Game{
         }
     }
 
-    public static int checkLetter(Scanner s, String[] letters, ArrayList<String> chosen){
+    public static int checkLetter(Scanner s, String[] letters, ArrayList<String> chosen, ArrayList<String> alph){
         while (true){
             String answer;
             System.out.println("Enter a letter");
             answer = s.nextLine().toUpperCase();
-            int inWord = 0;
-            System.out.println(letters);
-            for (String i:letters){
-                System.out.println(i);
-                if (answer.equals(i)){
-                    inWord = 1;
-                    System.out.println("inword");
-                    break;
+            if (alph.contains(answer.toUpperCase())){
+                int inWord = 0;
+                System.out.println(letters);
+                for (String i:letters){
+                    System.out.println(i);
+                    if (answer.equals(i)){
+                        inWord = 1;
+                        break;
+                    }
                 }
-            }
-            if (inWord==0){
-                System.out.println("false");
-                return 0;
-            }else {
-                if (chosen.contains(answer)){
-                    System.out.println("Letter already chosen");
-                }else{
-                    System.out.println("true");
-                    chosen.add(answer);
-                    return 1;
+                if (inWord==0){
+                    return 0;
+                }else {
+                    if (chosen.contains(answer)){
+                        System.out.println("Letter already chosen");
+                    }else{
+                        chosen.add(answer);
+                        return 1;
+                    }
+                    
                 }
-                
             }
         }
     }
@@ -77,7 +76,7 @@ public class Game{
         String word = wordObj.accessWord().toUpperCase();
         String[] letters = word.split("");
         System.out.println(word);
-        System.out.println(letters);
+        //System.out.println(letters);
         String blankWord = "";
         for (int i=0; i<word.length(); i++){
             blankWord += "_ ";
@@ -89,12 +88,16 @@ public class Game{
         String stage = man.newStage();
         System.out.println(stage);
         System.out.println(blankWord);
-        String[] alphabet = new String[]{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+        String[] alphs = new String[]{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+        ArrayList<String> alphabet = new ArrayList<String>();
+        for (String a:alphs){
+            alphabet.add(a);
+        }
         ArrayList<String> chosen = new ArrayList<String>();
         Scanner scanner = new Scanner(System.in);
         int i = 0;
         while (i!=word.length()){
-            int correct = checkLetter(scanner, letters, chosen);
+            int correct = checkLetter(scanner, letters, chosen, alphabet);
             if (correct==1){
                 System.out.println("Correct!");
                 i++;
