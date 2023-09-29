f0366c5ab95e0c7ddd9474293feba540ae1db88e
xinpengg <xinpengg@nycstudents.net>
2023-09-29 12:24:24 -0400

    .

--- src/DecisionTree.java
+++ src/DecisionTree.java
@@ -1,6 +1,25 @@
 public class DecisionTree
 {
     
-        
+    public void record(ArrayList<String> list) {
+        HashMap<String, List<String>> wordFamilies = new HashMap<>();
 
+        for (String word = words) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < word.length(); i++) {
+                sb.append(word.charAt(i)).append(",");
+            }
+            String key = sb.toString();
+
+            List<String> family = wordFamilies.getOrDefault(key, new ArrayList<>());
+            family.add(word);
+            wordFamilies.put(key, family);
+        }
+
+
+        return wordFamilies;
+    }
+    }
 }
+
+
