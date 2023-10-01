f9386c0296205f70709a2a43662952a496ac3efc
xinpengg <xinpengg@nycstudents.net>
2023-10-01 00:00:23 -0400

    add parameter  thing

--- src/DecisionTree.java
+++ src/DecisionTree.java
@@ -1,14 +1,14 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class DecisionTree
-{
-    
-    public void record(ArrayList<String> list) {
+public class DecisionTree {
+
+    public Map<String, List<String>> record(ArrayList<String> list) {
         HashMap<String, List<String>> wordFamilies = new HashMap<>();
 
-        for (String word: list) {
+        for (String word : list) {
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < word.length(); i++) {
                 sb.append(word.charAt(i));
@@ -23,7 +23,7 @@ public class DecisionTree
 
         return wordFamilies;
     }
-    }
 }
+
 
 
