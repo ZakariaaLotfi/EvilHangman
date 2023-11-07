208545e30293f14eca8efdf9da10eea569b3af8e
xinpengg <144473684+xinpengg@users.noreply.github.com>
2023-11-07 17:28:02 -0500

    git broke so manual commit??

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,9 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 
 class Main {
     public static void main(String[] args) throws IOException, InterruptedException, UnsupportedAudioFileException, LineUnavailableException {
+      List<String> words = Arrays.asList("ALLY", "BETA" ,"COOL" ,"DEAL" ,"ELSE" ,"FLEW", "GOOD", "HOPE", "IBEX");
+      EvilHangman game2 = new EvilHangman(words);
+      game2.playGame();
       Game game = new Game();
     }
   }
