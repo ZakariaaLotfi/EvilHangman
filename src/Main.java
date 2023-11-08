8195badae6cbeda1843582f51a7bdcbca85e88d1
xinepeng <smartpeoplelosesp@gmail.com>
2023-11-07 20:21:43 -0500

    ok workin on node stuff now

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,10 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
 
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
-class Main {
+public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException, UnsupportedAudioFileException, LineUnavailableException {
 
